a4e5793d5b9477c3db397c4fcde9e34249b6a900
phoenix0xff <725071+phoenix0xff@users.noreply.github.com>
2025-10-11 18:05:54 +0200

    l

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('new2: Application Changes successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes');
+  res.send('new4: Application Changes successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes');
 });
 
 app.listen(port, () => {
